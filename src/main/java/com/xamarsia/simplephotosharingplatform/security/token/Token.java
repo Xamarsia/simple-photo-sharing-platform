eb0ba5471de2de1b4305029f46b1ae5482c662ac
xamarsia <xamarsia@gmail.com>
2023-10-12 12:34:07 -0400

    [SPSP-19] Implement Token

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/security/token/Token.java
@@ -0,0 +1,30 @@
+package com.xamarsia.simplephotosharingplatform.security.token;
+
+import com.xamarsia.simplephotosharingplatform.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    @ManyToOne(cascade = CascadeType.DETACH)
+    @JoinColumn(name = "user_id")
+    public User user;
+}
---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.xamarsia.simplephotosharingplatform.security.token;
+
+public enum TokenType {
+    BEARER
+}
