4feeba09f6f20ecaca19dac4f540e3b104acb401
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2023-10-12 14:01:55 -0400

    [SPSP-17] Add JwtService

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/security/ApplicationConstants.java
@@ -0,0 +1,12 @@
+package com.xamarsia.simplephotosharingplatform.security;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApplicationConstants {
+    @UtilityClass
+    public class Validation {
+
+        public static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    }
+}
