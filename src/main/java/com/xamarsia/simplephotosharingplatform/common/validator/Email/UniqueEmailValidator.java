44b8ecc36d46e7b5b9baea11363828bcb89bf9cc
xamarsia <xamarsia@gmail.com>
2023-10-12 02:20:47 -0400

    [SPSP-11] Implement unique email validator

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/common/validator/Email/UniqueEmailValidator.java
@@ -0,0 +1,20 @@
+package com.xamarsia.simplephotosharingplatform.common.validator.Email;
+
+import com.xamarsia.simplephotosharingplatform.UserService;
+import lombok.NonNull;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+@AllArgsConstructor
+@Component
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    private final UserService service;
+
+    @Override
+    public boolean isValid(@NonNull final String email, ConstraintValidatorContext context) {
+        return !service.isEmailUsed(email);
+    }
+}
