f122abd6d3f7aa7b387aff054547589061209259
xamarsia <xamarsia@gmail.com>
2023-10-12 12:05:14 -0400

    [SPSP-10] Add AuthenticationResponse DTO

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/dto/auth/AuthenticationResponse.java
@@ -0,0 +1,10 @@
+package com.xamarsia.simplephotosharingplatform.dto.auth;
+
+import com.xamarsia.simplephotosharingplatform.UserDTO;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record AuthenticationResponse(
+        @NotNull @NotEmpty String token,
+        @NotNull @NotEmpty UserDTO userDTO) {
+}
