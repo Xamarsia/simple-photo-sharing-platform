600db4ae1c168c11a0f7c0dfcef51a3bfe2dd22c
xamarsia <xamarsia@gmail.com>
2023-10-12 02:26:31 -0400

    [SPSP-8] Add RegisterRequest DTO

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/dto/auth/RegisterRequest.java
@@ -0,0 +1,57 @@
+package com.xamarsia.simplephotosharingplatform.dto.auth;
+
+import com.xamarsia.simplephotosharingplatform.common.validator.Email.UniqueEmail;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class RegisterRequest {
+    @UniqueEmail
+    @Size(min = 1, message = "Email is required.")
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String fullName;
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    @NotNull(message = "Password is required.")
+    @Size(min = 6, message = "Password should be at least 6 characters.")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
