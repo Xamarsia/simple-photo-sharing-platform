1485d7a81b29ab794633eed27296c811da2705ba
xamarsia <xamarsia@gmail.com>
2023-10-12 02:01:13 -0400

    [SPSP-14] Implement UserDTOMapper

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/UserDTOMapper.java
@@ -0,0 +1,26 @@
+package com.xamarsia.simplephotosharingplatform;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDTOMapper implements Function<User, UserDTO> {
+    @Override
+    public UserDTO apply(User user) {
+        UserDTO userDTO = new UserDTO(
+                user.getId(),
+                user.getFullName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getProfileImageId(),
+                user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList())
+        );
+        return userDTO;
+    }
+}
