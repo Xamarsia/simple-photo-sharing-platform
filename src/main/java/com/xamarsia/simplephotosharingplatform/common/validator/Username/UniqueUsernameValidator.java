223bbf1c9ccffba69f26faea708db5281631686d
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2023-11-30 00:07:46 -0500

    [SPSP-55] Add UniqueUsername annotation

---
+++ src/main/java/com/xamarsia/simplephotosharingplatform/common/validator/Username/UniqueUsernameValidator.java
@@ -0,0 +1,20 @@
+package com.xamarsia.simplephotosharingplatform.common.validator.Username;
+
+import com.xamarsia.simplephotosharingplatform.user.UserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+
+
+@AllArgsConstructor
+@Component
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+    private final UserService service;
+
+    @Override
+    public boolean isValid(@NonNull final String username, ConstraintValidatorContext context) {
+        return !service.isUsernameUsed(username);
+    }
+}
