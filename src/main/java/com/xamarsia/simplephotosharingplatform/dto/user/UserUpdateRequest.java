5d9e8c07cba8dd79bf28de6bcc7169a46582f939
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2024-01-01 20:21:28 -0500

    [SPSP-70] Remove password from user update request

--- src/main/java/com/xamarsia/simplephotosharingplatform/dto/user/UserUpdateRequest.java
+++ src/main/java/com/xamarsia/simplephotosharingplatform/dto/user/UserUpdateRequest.java
@@ -3,7 +3,9 @@ package com.xamarsia.simplephotosharingplatform.dto.user;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
 public class UserUpdateRequest {
 
     //TODO Move the email update logic to a separate request with a request for e-mail confirmation
@@ -19,41 +21,4 @@ public class UserUpdateRequest {
     @NotNull
     @NotEmpty
     private String username;
-
-    @NotEmpty
-    @NotNull(message = "Password is required.")
-    @Size(min = 6, message = "Password should be at least 6 characters.")
-    private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
