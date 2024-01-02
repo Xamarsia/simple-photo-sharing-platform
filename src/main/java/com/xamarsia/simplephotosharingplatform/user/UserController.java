8a2a7521359fad56122871213bfca56825a4f585
Xamarsia <67323429+Xamarsia@users.noreply.github.com>
2024-01-01 19:37:44 -0500

    [SPSP-68] Remove ID from user update request

--- src/main/java/com/xamarsia/simplephotosharingplatform/user/UserController.java
+++ src/main/java/com/xamarsia/simplephotosharingplatform/user/UserController.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.CollectionModel;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -28,36 +29,30 @@ public class UserController {
     private final JwtService jwtService;
 
     @GetMapping("/")
-    public ResponseEntity<UserDTO> getAuthenticatedUser(Authentication authentication){
+    public ResponseEntity<UserDTO> getAuthenticatedUser(Authentication authentication) {
         UserDTO userDTO = userDTOMapper.apply(service.getAuthenticatedUser(authentication));
-        return ResponseEntity.ok()
-                .body(userDTO);
+        return ResponseEntity.ok().body(userDTO);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserDTOById(@PathVariable Long id) {
         User user = service.getById(id);
         UserDTO userDTO = userDTOMapper.apply(user);
-        return ResponseEntity.ok()
-                .body(userDTO);
+        return ResponseEntity.ok().body(userDTO);
     }
 
     @GetMapping("/all")
     public CollectionModel<EntityModel<UserDTO>> all() {
-        List<EntityModel<UserDTO>> users = service.selectAllUsers().stream().map(user ->  EntityModel.of(
-                        userDTOMapper.apply(user),
-                        linkTo(methodOn(UserController.class).getUserDTOById(user.getId())).withSelfRel()))
-                .collect(Collectors.toList());
+        List<EntityModel<UserDTO>> users = service.selectAllUsers().stream().map(user -> EntityModel.of(userDTOMapper.apply(user), linkTo(methodOn(UserController.class).getUserDTOById(user.getId())).withSelfRel())).collect(Collectors.toList());
 
         return CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
     }
 
-    @PutMapping("/{id}")
-    ResponseEntity<?> updateUser(@RequestBody @Valid UserUpdateRequest newUserData, @PathVariable Long id) {
-        User updatedUser = service.updateUser(newUserData, id);
+    @PutMapping("/update")
+    ResponseEntity<?> updateUser(Authentication authentication, @RequestBody @Valid UserUpdateRequest newUserData) {
+        User updatedUser = service.updateUser(authentication, newUserData);
         UserDTO userDTO = userDTOMapper.apply(updatedUser);
-        return ResponseEntity.ok()
-                .body(userDTO);
+        return ResponseEntity.ok().body(userDTO);
     }
 
     @PutMapping("/updateImage/{id}")
@@ -68,6 +63,6 @@ public class UserController {
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         service.deleteUserById(id);
-       return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
