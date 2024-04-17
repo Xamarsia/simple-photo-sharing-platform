f1023986968da27e887740a09d1f38f388903cc9
xamarsia <xamarsia@gmail.com>
2024-04-17 16:50:59 -0400

    Code refactoring

--- src/main/java/com/xamarsia/simplephotosharingplatform/security/config/LogoutService.java
+++ src/main/java/com/xamarsia/simplephotosharingplatform/security/config/LogoutService.java
@@ -2,12 +2,13 @@ package com.xamarsia.simplephotosharingplatform.security.config;
 
 import com.xamarsia.simplephotosharingplatform.exception.exceptions.InvalidJWTException;
 import com.xamarsia.simplephotosharingplatform.exception.exceptions.UnauthorizedAccessException;
-import com.xamarsia.simplephotosharingplatform.security.AuthenticationConstants;
 import com.xamarsia.simplephotosharingplatform.security.token.Token;
 import com.xamarsia.simplephotosharingplatform.security.token.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
-
     private final TokenRepository tokenRepository;
+
+    @Value("${application.security.token-type}")
+    private String tokenType;
 
     @Override
     public void logout(
@@ -25,13 +28,12 @@ public class LogoutService implements LogoutHandler {
             HttpServletResponse response,
             Authentication authentication
     ) {
-        final String authHeader = request.getHeader(AuthenticationConstants.Validation.AUTHORIZATION);
-
-        if (authHeader == null || !authHeader.startsWith(AuthenticationConstants.Validation.BEARER)) {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith(tokenType)) {
             throw new UnauthorizedAccessException("[logout]: Logout failed. User is not authorized.");
         }
 
-        final String jwt = authHeader.substring(AuthenticationConstants.Validation.BEARER.length());
+        final String jwt = authHeader.substring(tokenType.length());
 
         Token token = tokenRepository.findByToken(jwt)
                 .orElseThrow(() -> new InvalidJWTException("[logout]: Logout failed. Token not found in repository."));
